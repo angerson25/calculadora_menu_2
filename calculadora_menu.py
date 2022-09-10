223d30e323677e23a134694019d9ef3b949fbdad
angerson25 <angerson2224639@correo.uis.edu.co>
2022-09-10 08:29:54 -0500

    Fin

--- calculadora_menu.py
+++ calculadora_menu.py
@@ -21,3 +21,42 @@ print("5. Potencia (El primero a la potencia del segundo)")
 print("6. Logaritmo (El logaritmo del primero)")
 
 opcion = int(input("\nDame la opción: "))
+
+#PROCESAMIENTO
+if(opcion == 1):
+    z = x + y
+    print(x, "+" ,y)
+elif (opcion==2):
+    z = x - y
+    print(x,"+" ,y)
+elif (opcion==3):
+    z = x * y
+    print(x,"X" ,y)
+elif (opcion==4 and y!=0):
+    z = x / y
+    print(x, "/" ,y)
+elif (opcion==4 and y==0):
+    print("El denominador es igual a 0")
+    print("NO se puede realizar la division")
+elif (opcion == 5):
+    z = pow(x,y)
+    print(x,"^",y)
+elif (opcion == 6 and x > 0):
+    z = log(x)
+    print("logaritmo de", x)
+elif (opcion == 6 and x <= 0):
+    print("El valor de x es <= a cero y")
+    print("NO se puede calcular el logaritmo.")
+    bandera = True
+else:
+    print("Opcion no válida")
+
+# se escribe el resutado con otra condición
+if (opcion < 7 and bandera == False):
+    print("Resultado = ", z)
+
+# fin
+
+
+
+
